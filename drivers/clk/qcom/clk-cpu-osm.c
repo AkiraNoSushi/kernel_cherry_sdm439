635244fe71827b46160e1db79b0639421db68bfc
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-10-16 19:44:31 +0530

    clk: qcom: clk-cpu-osm: Set dvfs_possible_from_any_cpu cpufreq driver flag
    As remote cpufreq updates are supported on MSM platforms, set
    dvfs_possible_from_any_cpu cpufreq driver flag.
    
    Change-Id: I0ae9f130ad37654967ce754390152a9dbd0ec80b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -812,6 +812,7 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	policy->cpuinfo.transition_latency = MIN_RATE_LIMIT_US;
+	policy->dvfs_possible_from_any_cpu = true;
 	policy->driver_data = c;
 	return 0;
 
