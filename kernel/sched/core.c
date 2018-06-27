f06c1dfb9b16bc904f8573d46dba0a0a341ea4ed
Connor O'Brien <connoro@google.com>
2018-06-26 17:45:55 -0700

    sched: cpufreq: stop ignoring util updates
    Eliminate the check for SCHED_CPUFREQ_WALT flag in
    cpufreq_update_util(), update calling code to stop using that flag,
    and replace its definition with a placeholder.
    
    Test: Trace shows more frequent util updates
    Bug: 110604715
    Change-Id: I3b74e950b984194f08ecbbf91872467a200c9d1d
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2933,9 +2933,7 @@ out:
 	if (success && sched_predl) {
 		raw_spin_lock_irqsave(&cpu_rq(cpu)->lock, flags);
 		if (do_pl_notif(cpu_rq(cpu)))
-			cpufreq_update_util(cpu_rq(cpu),
-					    SCHED_CPUFREQ_WALT |
-					    SCHED_CPUFREQ_PL);
+			cpufreq_update_util(cpu_rq(cpu), SCHED_CPUFREQ_PL);
 		raw_spin_unlock_irqrestore(&cpu_rq(cpu)->lock, flags);
 	}
 
@@ -3981,7 +3979,6 @@ void scheduler_tick(void)
 	bool early_notif;
 	u32 old_load;
 	struct related_thread_group *grp;
-	unsigned int flag = 0;
 
 	sched_clock_tick();
 
@@ -4000,9 +3997,8 @@ void scheduler_tick(void)
 
 	early_notif = early_detection_notify(rq, wallclock);
 	if (early_notif)
-		flag = SCHED_CPUFREQ_WALT | SCHED_CPUFREQ_EARLY_DET;
+		cpufreq_update_util(rq, SCHED_CPUFREQ_EARLY_DET);
 
-	cpufreq_update_util(rq, flag);
 
 	psi_task_tick(rq);
 
