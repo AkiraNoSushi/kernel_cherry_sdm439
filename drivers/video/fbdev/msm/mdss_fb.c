7b2ebc307d668c26fdc2d8b3d5c2da1bed7c03ac
flar2 <asegaert@gmail.com>
2019-03-16 13:50:55 +0800

    KLAPSE: adapt KLAPSE
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -65,6 +65,10 @@ extern uint32_t white_point_num_g;
 extern uint32_t white_point_num_b;
 #endif
 
+#ifdef CONFIG_KLAPSE
+#include "klapse.h"
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -316,6 +320,9 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		mdss_fb_set_backlight(mfd, bl_lvl);
 		mutex_unlock(&mfd->bl_lock);
 	}
+#ifdef CONFIG_KLAPSE
+	set_rgb_slider(bl_lvl);
+#endif
 }
 
 static enum led_brightness mdss_fb_get_bl_brightness(
