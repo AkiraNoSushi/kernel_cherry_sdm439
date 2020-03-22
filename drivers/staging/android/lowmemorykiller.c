02e30b68a5bd6aced3c11cac141e87f0d9244ff9
psndna88 <psndna88@gmail.com>
2020-03-22 22:14:50 +0530

    lmk: force disable adaptive lmk
    Apparently this is causing our too frequent cleansing of memory
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -255,8 +255,10 @@ static short adj_max_shift = 353;
 module_param_named(adj_max_shift, adj_max_shift, short, 0644);
 
 /* User knob to enable/disable adaptive lmk feature */
-static int enable_adaptive_lmk;
-module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int, 0644);
+static int enable_adaptive_lmk = 0;
+static int enable_adaptive_lmk_dummy;
+module_param_named(enable_adaptive_lmk, enable_adaptive_lmk_dummy, int,
+		   S_IRUGO | S_IWUSR);
 
 /*
  * This parameter controls the behaviour of LMK when vmpressure is in
