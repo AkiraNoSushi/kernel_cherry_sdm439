4fd38c767d73c0744b4124a21e323e0fe2dd7700
Lau <laststandrighthere@gmail.com>
2020-06-05 10:16:25 +0800

    schedutil: Initialize freq. rate limits with 0 only when using WALT
    Initialize with schedutil default values when using PELT.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -797,6 +797,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
+#ifdef CONFIG_SCHED_WALT
 	/*
 	 * NOTE:
 	 * intializing up_rate/down_rate to 0 explicitly in kernel
@@ -804,6 +805,11 @@ static int sugov_init(struct cpufreq_policy *policy)
 	 */
 	tunables->up_rate_limit_us = 5000;
 	tunables->down_rate_limit_us = 20000;
+#else
+	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+#endif
+	tunables->iowait_boost_enable = true;
 
 	tunables->iowait_boost_enable = true;
 	policy->governor_data = sg_policy;
