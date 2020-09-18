c834bc15021fa71e3d01abfc8e2e18220ecd668b
TheSync <repo-sync@outlook.com>
2020-09-18 18:43:15 +0000

    drivers: staging: prima: Fix undeclared identifier of ANI_AKM_TYPE_SAE
    ../drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c:1553:33: error: use of undeclared identifier 'ANI_AKM_TYPE_SAE'; did you mean 'ANI_AKM_TYPE_OWE'?
                    if (akm_type == ANI_AKM_TYPE_SAE) {
                                    ^~~~~~~~~~~~~~~~
                                    ANI_AKM_TYPE_OWE
    ../drivers/staging/prima/CORE/MAC/inc/aniSystemDefs.h:115:5: note: 'ANI_AKM_TYPE_OWE' declared here
        ANI_AKM_TYPE_OWE,
        ^
    
    Signed-off-by: TheSync <repo-sync@outlook.com>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAssocReqFrame.c
@@ -1550,7 +1550,7 @@ limProcessAssocReqFrame(tpAniSirGlobal pMac, tANI_U8 *pRxPacketInfo,
                 akm_type = lim_translate_rsn_oui_to_akm_type(
                                     Dot11fIERSN.akm_suite[0]);
 
-                if (akm_type == ANI_AKM_TYPE_SAE) {
+                if (akm_type == ANI_AKM_TYPE_OWE) {
                     if (eSIR_SUCCESS != (status =
                         lim_check_sae_pmf_cap(psessionEntry, &Dot11fIERSN))) {
                         /* Reject pmf disable SAE STA */
