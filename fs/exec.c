bce96c0c4a087a34ccc2be8d041f03d55524e83d
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-24 00:44:02 -0800

    kernel: Affine hwcomposer to big CPUs
    HWC in Android is responsible for communicating userspace's graphics
    requests with the kernel. Affining it to fast CPUs greatly reduces
    jitter.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [@Jprimero15: Adapted a bit for 4.9]
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -72,6 +72,7 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct task_struct *zygote32_task;
@@ -1809,6 +1810,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 			zygote32_task = current;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_task = current;
+		else if (unlikely(!strncmp(filename->name,
+					   HWCOMPOSER_BIN_PREFIX,
+					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
 	}
 
 	/* execve succeeded */
