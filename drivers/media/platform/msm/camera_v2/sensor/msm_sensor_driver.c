fd4f9270126e63e64a3bb47afe818ffb1b21cc03
SimplyJoel <thegame455iewolf@gmail.com>
2021-02-07 20:20:25 +0000

    drivers: camera_v2: unify olivelite with olive and olivewood
    This makes it possible for a single build to work on all olive variants.
    
    Signed-off-by: SimplyJoel <thegame455iewolf@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -748,7 +748,7 @@ static int32_t msm_sensor_driver_is_special_support(
 }
 
 // for olive caemra hw info
-#if defined OLIVE_MSM_CAMERA_HW_INFO || defined OLIVEWOOD_MSM_CAMERA_HW_INFO
+#if defined OLIVE_MSM_CAMERA_HW_INFO || defined OLIVEWOOD_MSM_CAMERA_HW_INFO || defined OLIVELITE_MSM_CAMERA_HW_INFO
 	uint32_t i = 0;
 	char olive_rear_camera_str_buff[6][2][20] = {
 		{"olive_imx486_ofilm", "sony_imx486_i"},
@@ -770,23 +770,6 @@ static int32_t msm_sensor_driver_is_special_support(
 		{"olive_gc02m1_sunny", "galaxycore_02m1_iii"},
 		{"olive_gc02m1b_sunny", "galaxycore_02m1_iiii"},
 
-	};
-#endif
-
-#ifdef OLIVELITE_MSM_CAMERA_HW_INFO
-	uint32_t i = 0;
-	char olive_rear_camera_str_buff[6][2][20] = {
-		{"olive_imx486_ofilm", "sony_imx486_i"},
-		{"olive_imx486_sunny", "sony_imx486_ii"},
-		{"olive_imx363_qtech", "sony_imx363_i"},
-		{"olive_s5k2l7_sunny", "samsung_s5k2l7_ii"},
-		{"olive_ov13b10_qtech", "omnivision_13b10_i"},
-		{"olive_ov13b10_ofilm", "omnivision_13b10_ii"},
-	};
-	char olive_front_camera_str_buff[3][2][20] = {
-		{"olive_ov8856_qtech", "omnivision_8856_ii"},
-		{"olive_ov8856_sunny", "omnivision_8856_i"},
-		{"olive_s5k4h7_sunny", "samsung_s5k4h7_iii"},
 	};
 #endif
 
