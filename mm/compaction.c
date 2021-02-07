8b396b3e921aeefabbe506c4b77431e09e968b36
Tyler Nijmeh <tylernij@gmail.com>
2021-02-07 09:08:59 +0000

    mm/compaction: Prioritize compaction work
    We want compaction work to at the very least exceed SCHED_NORMAL and
    SCHED_OTHER tasks. Bump kcompactd to a kthread with SCHED_FIFO and
    priority 1.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -21,6 +21,7 @@
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
 #include <linux/psi.h>
+#include <linux/sched.h>
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -1996,6 +1997,9 @@ static int kcompactd(void *p)
 	struct task_struct *tsk = current;
 
 	const struct cpumask *cpumask = cpumask_of_node(pgdat->node_id);
+	struct sched_param param = { .sched_priority = 1 };
+
+	sched_setscheduler(current, SCHED_FIFO, &param);
 
 	if (!cpumask_empty(cpumask))
 		set_cpus_allowed_ptr(tsk, cpumask);
