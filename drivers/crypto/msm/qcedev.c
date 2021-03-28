5716e1a621f6715c100b4fcab3deccc557e34b44
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-03-28 11:07:19 +0000

    drivers/crypto: Import Xiaomi changes

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -2,6 +2,7 @@
  * QTI CE device driver.
  *
  * Copyright (c) 2010-2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
