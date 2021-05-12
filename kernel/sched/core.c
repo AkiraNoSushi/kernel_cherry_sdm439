ccb8f15d7299b694bacacd8c8495c16803b9daf6
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-05-12 03:13:47 -0400

    Revert "kernel: Add API to mark IRQs and kthreads as performance critical"
    This reverts commit a4c3fe5df6e0e5c649ca44f3faa1b642b0e9b791.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1848,16 +1848,6 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
-static const struct cpumask *adjust_cpumask(struct task_struct *p,
-					    const struct cpumask *req_mask)
-{
-	/* Force all performance-critical kthreads onto the big cluster */
-	if (p->flags & PF_PERF_CRITICAL)
-		return cpu_perf_mask;
-
-	return req_mask;
-}
-
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
 	struct rq *rq = task_rq(p);
@@ -1905,8 +1895,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	struct rq *rq;
 	int ret = 0;
 	cpumask_t allowed_mask;
-
-	new_mask = adjust_cpumask(p, new_mask);
 
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
