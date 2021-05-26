11457609a039abd5da5e7771b7ecffcc2a18c9a8
Nathan Chancellor <natechancellor@gmail.com>
2021-05-26 02:54:47 -0400

    staging: lowmemorykiller: Fix enum conversion warning
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -313,7 +313,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 	if (pressure >= 95) {
 		other_file = global_node_page_state(NR_FILE_PAGES) -
 			global_node_page_state(NR_SHMEM) -
-			global_page_state(NR_UNEVICTABLE) -
+			global_node_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 		other_free = global_page_state(NR_FREE_PAGES);
 
@@ -327,7 +327,7 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 
 		other_file = global_node_page_state(NR_FILE_PAGES) -
 			global_node_page_state(NR_SHMEM) -
-			global_page_state(NR_UNEVICTABLE) -
+			global_node_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 
 		other_free = global_page_state(NR_FREE_PAGES);
@@ -647,7 +647,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 #else
 	other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 
-	if (global_page_state(NR_SHMEM) + global_page_state(NR_UNEVICTABLE) + total_swapcache_pages() <
+	if (global_node_page_state(NR_SHMEM) + global_node_page_state(NR_UNEVICTABLE) + total_swapcache_pages() <
 			global_node_page_state(NR_UNEVICTABLE) <
 			global_node_page_state(NR_FILE_PAGES))
 		other_file = global_node_page_state(NR_FILE_PAGES) -
