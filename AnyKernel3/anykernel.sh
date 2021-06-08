bd1b9febdf0138b482f2f5169efa9899e4a48ad9
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-06-08 05:38:08 -0400

    anykernel: Patch WiFi HAL to work with built-in Prima
    MIUI vendor blobs and some custom ROMs require Prima to be available as a kernel module.
    This commit patches WiFi HAL to stop it from loading Prima's kernel module.

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -41,11 +41,37 @@ mount -o rw,remount /system
 
 # Patches
 # Prevent init from overriding kernel tweaks.
-ui_print "Patching init..."
+ui_print "Patching system's init..."
 # IMO this is kinda destructive but works
 find /system/etc/init/ -type f | while read file; do 
 sed -Ei 's;[^#](write /proc/sys/(kernel|vm)/(sched|dirty|perf_cpu|page-cluster|stat|swappiness|vfs));#\1;g' $file
 done
+# Patch WiFI HAL
+ui_print "Detecting WiFI HAL..."
+wifi_hal=$(find /vendor/lib64 /vendor/lib -name "libwifi-hal.so" | head -n 1)
+if grep -q "pronto_wlan.ko" $wifi_hal; then
+    ui_print "Patching WiFI HAL..."
+    func_hex_offset=$(./tools/readelf $wifi_hal -sW | grep "is_wifi_driver_loaded" | awk '{print $2}')
+    func_dec_offset=$(printf "%d" "0x"$func_hex_offset)
+    hal_arch=$(./tools/readelf -h $wifi_hal | grep "Class" | awk '{print $2}')
+    if [ $hal_arch == "ELF64" ]; then
+        # patch:
+        #   mov w0, #0x1
+        #   ret
+        printf '\x20\x00\x80\x52\xc0\x03\x5f\xd6' | ./tools/busybox dd of=$wifi_hal bs=1 seek=$func_dec_offset conv=notrunc status=none
+    else
+        # patch:
+        #   mov r0, #0x1
+        #   mov pc, lr
+        # We must substract 1 to function address because of Thumb's least-significant bit
+        printf '\x01\x20\xf7\x46' | ./tools/busybox dd of=$wifi_hal bs=1 seek=$[$func_dec_offset-1] conv=notrunc status=none
+    fi
+    # Give WiFi HAL fwpath sysfs privileges
+    ui_print "Patching vendor's init..."
+    insert_line /vendor/etc/init/hw/init.qcom.rc "chown wifi wifi /sys/module/wlan/parameters/fwpath" after "    chmod 0660 /sys/kernel/dload/dload_mode" $(printf "\n    chown wifi wifi /sys/module/wlan/parameters/fwpath")
+else
+    ui_print "No WiFi HAL patching needed."
+fi
 # IORap
 ui_print "Patching system's build.prop..."
 patch_prop /system/build.prop "ro.iorapd.enable" "true"
