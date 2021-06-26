b8fa99cd946d49dd93b92e042216e1670c87a7a9
Joel Gómez <nahuelgomez329@gmail.com>
2021-06-26 10:21:45 +0000

    .drone.sh: use LLVM tools as replacements for some binutils binaries
    Taken from Proton Clang documentation:
    https://github.com/kdrag0n/proton-clang/blob/master/README.md#building-linux
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- .drone.sh
+++ .drone.sh
@@ -13,6 +13,11 @@ make -j$(nproc --all) O=out ARCH=arm64 \
                       CC=clang \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip \
 
 # Build flashable zip
 cp out/arch/arm64/boot/dtbo.img AnyKernel3/
