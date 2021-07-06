5dbfd4da11221c15e066bbaea604020ca3f5ed84
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-07-06 14:50:33 -0400

    arm64: Add Xiaomi SDM439 device and features detection code
    This works as a better, drop-in replacement to legacy Xiaomi SDM439 detection driver.
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>}
    [Changed Kconfig description and name]

---
+++ include/linux/sdm439.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+// Author: @AkiraNoSushi
+
+#ifndef _LINUX_SDM439_H
+#define _LINUX_SDM439_H
+
+enum SDM439_devices {
+    DEVICE_UNKNOWN = -1,
+    XIAOMI_PINE,
+    XIAOMI_OLIVES
+};
+
+extern enum SDM439_devices sdm439_devices;
+
+extern int sdm439_current_device;
+
+extern bool legacy_omx;
+
+#endif
