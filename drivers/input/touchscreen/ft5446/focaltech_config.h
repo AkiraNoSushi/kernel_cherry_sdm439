003205c28e60bbc768f91445633f4b2d420e2c6c
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-11-30 00:53:11 -0400

    drivers: input: touchscreen: ft5446: disable debug mode and self tests

--- drivers/input/touchscreen/ft5446/focaltech_config.h
+++ drivers/input/touchscreen/ft5446/focaltech_config.h
@@ -111,7 +111,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN                            1
+#define FTS_DEBUG_EN                            0
 
 /*
  * Linux MultiTouch Protocol
@@ -135,19 +135,19 @@
  * ESD check & protection
  * default: disable
  */
-#define FTS_ESDCHECK_EN                         1
+#define FTS_ESDCHECK_EN                         0
 
 /*
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Tp_self test enable
  * 1: enable, 0:disable(default)
  */
- #define FTS_TP_SELFTEST                     1
+ #define FTS_TP_SELFTEST                     0
 
 /*
  * Glove mode enable
