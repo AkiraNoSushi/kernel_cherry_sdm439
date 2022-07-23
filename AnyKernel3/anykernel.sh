e3f9f8aa3b965bb096ef52541dca32b75c68b991
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2022-07-23 01:52:10 -0400

    🍒CherryKernel V2.7.3

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Cherry Kernel V2.7.2 - by @AkiraNoSushi & @Flopster101
+kernel.string=Cherry Kernel V2.7.3 - by @AkiraNoSushi & @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=0
